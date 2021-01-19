41977eb3ce195694138409288d9b63a634bcb2a9
AdinGRT <adingrt@gmail.com>
2021-01-19 14:46:11 -0600

    se crearon las clases de dispositivo de entrada

---
+++ src/com/gm/mundopc/Raton.java
@@ -0,0 +1,15 @@
+package com.gm.mundopc;
+
+/**
+ *
+ * @author adingrt
+ */
+public class Raton extends DispositivoEntrada {
+    int idRaton;
+    int contadorRatones;
+
+    public Raton(String tipoEntrada, String marca) {
+        super(tipoEntrada, marca);
+    }
+
+}
---
+++ src/com/gm/mundopc/Teclado.java
@@ -0,0 +1,14 @@
+package com.gm.mundopc;
+
+/**
+ *
+ * @author adingrt
+ */
+public class Teclado extends DispositivoEntrada {
+    int idTeclado;
+    int contadorTeclados;
+
+    public Teclado(String tipoEntrada, String marca) {
+        super(tipoEntrada, marca);
+    }
+}
