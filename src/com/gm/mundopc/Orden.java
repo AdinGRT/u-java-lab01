012f44a772e81022e51b2646505bcbcb98494769
AdinGRT <adingrt@gmail.com>
2021-01-19 23:43:41 -0600

    se creo la clase Orden

---
+++ src/com/gm/mundopc/Orden.java
@@ -0,0 +1,14 @@
+package com.gm.mundopc;
+
+/**
+ *
+ * @author adingrt
+ */
+public class Orden {
+    int idOrden;
+    Computadora computadoras[];
+    int contadorOrdenes;
+    int contadorComputadora;
+    int maxComputadoras;
+    
+}
